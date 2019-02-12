8a6e6b66e6727c90786dff49848acce3df12b74c
jykym1006 <jykym1006@gmail.com>
2019-02-12 10:03:20 +0900

    Postモデルを追加

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -12,15 +12,15 @@ class Post extends AppModel {
  * @var array
  */
 	public $validate = array(
-		'title' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
+		'title' => [
+			'notEmpty' => [
+				'rule' => ['notEmpty'],
+				'message' => 'タイトルは必須入力です'
+			],
+			'maxLength' => [
+				'rule' => ['maxLength', '255'],
+				'message' => 'タイトルは255文字以内で入力してください',
+			]
+		],
 	);
 }

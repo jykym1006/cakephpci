1406eab67963e033a5953674b88a05a322854e5c
jykym1006 <jykym1006@gmail.com>
2019-02-12 15:49:06 +0900

    テストデータはFabricateを使って生成するように変更

--- app/Test/Fixture/PostFixture.php
+++ app/Test/Fixture/PostFixture.php
@@ -12,19 +12,4 @@ class PostFixture extends CakeTestFixture {
  */
 	public $import = array('model' => 'Post');
 
-/**
- * Records
- *
- * @var array
- */
-	public $records = array(
-		array(
-			'id' => 1,
-			'title' => 'Lorem ipsum dolor sit amet',
-			'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-			'created' => '2019-02-12 06:29:30',
-			'modified' => '2019-02-12 06:29:30'
-		),
-	);
-
 }
